73cc61f4954935b7570c0ea35e92fe80e5050dbf
endtrastic <a.tuvikene@voco.ee>
2025-01-13 13:34:53 +0200

    1 Ylesanne

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import './index.css'
+import Header from './components/Header'
+import Meals from './components/Meals' 
+
+
+
+
 const App = () => {
  return (
     <>
      <h1>Food Order App</h1>
+     <Meals/>
+     <Header/>
     </>
   );
 }
