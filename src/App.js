c5a157c0ebba809c347068e4ffae0c33c13ac42d
endtrastic <a.tuvikene@voco.ee>
2025-01-13 14:30:23 +0200

    Ylesanne 2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Header from './components/Header'
 import Meals from './components/Meals' 
 
 
-
+fetch('http://localhost:3001/meals')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Data:', data);
+});
 
 const App = () => {
  return (
     <>
      <h1>Food Order App</h1>
-     <Meals/>
+     <Meals />
      <Header/>
     </>
   );
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,10 +1,14 @@
+import React,{ useState, useEffect } from 'react';
+
+
+
 
 const Meals = () => {
+
     return (
         <ul id="meals">
             { 
-                
-            }
+            }   
         </ul>
     )
 }
